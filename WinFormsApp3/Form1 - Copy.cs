2e9f8a0898b4a8f5471c419862dd6f8b766b33a9
VuNgocPhuongNam <92594249+VuNgocPhuongNam@users.noreply.github.com>
2021-10-15 23:35:03 +0700

    my

---
+++ WinFormsApp3/Form1 - Copy.cs
@@ -0,0 +1,185 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WinFormsApp3
+{
+    public partial class maytinh : Form
+    {
+        public maytinh()
+        {
+            InitializeComponent();
+        }
+        bool nhapso = false;
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void txtso0_Click(object sender, EventArgs e)
+        {
+            if (nhapso)
+            {
+                txtketqua.Text = txtketqua.Text + "0";
+            }
+            else
+                txtketqua.Text = "0";
+            nhapso = true;
+        }
+
+        private void txtso1_Click(object sender, EventArgs e)
+        {
+            if (nhapso)
+            {
+                txtketqua.Text = txtketqua.Text + "1"; 
+            } else
+            txtketqua.Text = "1";
+            nhapso = true;
+        }
+
+        private void txtso2_Click(object sender, EventArgs e)
+        {
+            if (nhapso)
+            {
+                txtketqua.Text = txtketqua.Text + "2";
+            }
+            else
+                txtketqua.Text = "2";
+            nhapso = true;
+        }
+
+        private void txtso3_Click(object sender, EventArgs e)
+        {
+            if (nhapso)
+            {
+                txtketqua.Text = txtketqua.Text + "3";
+            }
+            else
+                txtketqua.Text = "3";
+            nhapso = true;
+        }
+
+        private void txtso1so4_Click(object sender, EventArgs e)
+        {
+            if (nhapso)
+            {
+                txtketqua.Text = txtketqua.Text + "4";
+            }
+            else
+                txtketqua.Text = "4";
+            nhapso = true;
+        }
+
+        private void txtso5_Click(object sender, EventArgs e)
+        {
+            if (nhapso)
+            {
+                txtketqua.Text = txtketqua.Text + "5";
+            }
+            else
+                txtketqua.Text = "5";
+            nhapso = true;
+        }
+
+        private void txtso6_Click(object sender, EventArgs e)
+        {
+            if (nhapso)
+            {
+                txtketqua.Text = txtketqua.Text + "6";
+            }
+            else
+                txtketqua.Text = "6";
+            nhapso = true;
+        }
+
+        private void txtso7_Click(object sender, EventArgs e)
+        {
+            if (nhapso)
+            {
+                txtketqua.Text = txtketqua.Text + "7";
+            }
+            else
+                txtketqua.Text = "7";
+            nhapso = true;
+        }
+
+        private void txtso8_Click(object sender, EventArgs e)
+        {
+            if (nhapso)
+            {
+                txtketqua.Text = txtketqua.Text + "8";
+            }
+            else
+                txtketqua.Text = "8";
+            nhapso = true;
+        }
+
+        private void txtso9_Click(object sender, EventArgs e)
+        {
+            if (nhapso)
+            {
+                txtketqua.Text = txtketqua.Text + "9";
+            }
+            else
+                txtketqua.Text = "9";
+            nhapso = true;
+        }
+        double sotruoc;
+        enum pheptinh { cong, tru,nhan,chia};
+        pheptinh Pheptinh;
+        private void nhappheptoan(object sender,EventArgs e)
+        {
+            Button btn = (Button)sender;
+            switch (btn.Text)
+            {
+                case "+":
+                    Pheptinh = pheptinh.cong;
+                    break;
+                case "-":
+                    Pheptinh = pheptinh.tru;
+                    break;
+                case "*":
+                    Pheptinh = pheptinh.nhan;
+                    break;
+                case "/":
+                    Pheptinh = pheptinh.chia;
+                    break;
+            }
+            sotruoc = double.Parse(txtketqua.Text);
+            nhapso = false;
+        }
+        private void tinhketqua()
+        {
+            double tam = double.Parse(txtketqua.Text);
+            double so = 0.0;
+            switch (Pheptinh)
+            {
+                case pheptinh.cong:
+                    so = sotruoc + tam;
+                    break;
+                case pheptinh.tru:
+                    so = sotruoc - tam;
+                    break;
+                case pheptinh.nhan:
+                    so = sotruoc * tam;
+                    break;
+                case pheptinh.chia:
+                    so = sotruoc / tam;
+                    break;
+            }
+            txtketqua.Text = so.ToString();
+        }
+
+        private void daubang_Click(object sender, EventArgs e)
+        {
+            tinhketqua();
+            nhapso =false;
+        }
+    }
+}
---
+++ WinFormsApp3/Form1.Designer.cs
@@ -0,0 +1,272 @@
+﻿
+namespace WinFormsApp3
+{
+    partial class maytinh
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.txtketqua = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.txtso0 = new System.Windows.Forms.Button();
+            this.daubang = new System.Windows.Forms.Button();
+            this.dauchia = new System.Windows.Forms.Button();
+            this.daunhan = new System.Windows.Forms.Button();
+            this.txtso1 = new System.Windows.Forms.Button();
+            this.txtso2 = new System.Windows.Forms.Button();
+            this.txtso3 = new System.Windows.Forms.Button();
+            this.dautru = new System.Windows.Forms.Button();
+            this.txtso1so4 = new System.Windows.Forms.Button();
+            this.txtso5 = new System.Windows.Forms.Button();
+            this.txtso6 = new System.Windows.Forms.Button();
+            this.daucong = new System.Windows.Forms.Button();
+            this.txtso7 = new System.Windows.Forms.Button();
+            this.txtso8 = new System.Windows.Forms.Button();
+            this.txtso9 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // txtketqua
+            // 
+            this.txtketqua.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
+            this.txtketqua.Location = new System.Drawing.Point(8, 20);
+            this.txtketqua.Name = "txtketqua";
+            this.txtketqua.Size = new System.Drawing.Size(318, 48);
+            this.txtketqua.TabIndex = 0;
+            this.txtketqua.TextAlign = System.Drawing.ContentAlignment.MiddleRight;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(12, 279);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(66, 52);
+            this.button1.TabIndex = 1;
+            this.button1.Text = ".";
+            this.button1.UseVisualStyleBackColor = true;
+            // 
+            // txtso0
+            // 
+            this.txtso0.Location = new System.Drawing.Point(84, 279);
+            this.txtso0.Name = "txtso0";
+            this.txtso0.Size = new System.Drawing.Size(66, 52);
+            this.txtso0.TabIndex = 2;
+            this.txtso0.Text = "0";
+            this.txtso0.UseVisualStyleBackColor = true;
+            this.txtso0.Click += new System.EventHandler(this.txtso0_Click);
+            // 
+            // daubang
+            // 
+            this.daubang.Location = new System.Drawing.Point(165, 279);
+            this.daubang.Name = "daubang";
+            this.daubang.Size = new System.Drawing.Size(66, 52);
+            this.daubang.TabIndex = 3;
+            this.daubang.Text = "=";
+            this.daubang.UseVisualStyleBackColor = true;
+            this.daubang.Click += new System.EventHandler(this.daubang_Click);
+            // 
+            // dauchia
+            // 
+            this.dauchia.Location = new System.Drawing.Point(250, 279);
+            this.dauchia.Name = "dauchia";
+            this.dauchia.Size = new System.Drawing.Size(66, 52);
+            this.dauchia.TabIndex = 4;
+            this.dauchia.Text = "/";
+            this.dauchia.UseVisualStyleBackColor = true;
+            this.dauchia.Click += new System.EventHandler(this.nhappheptoan);
+            // 
+            // daunhan
+            // 
+            this.daunhan.Location = new System.Drawing.Point(250, 208);
+            this.daunhan.Name = "daunhan";
+            this.daunhan.Size = new System.Drawing.Size(66, 52);
+            this.daunhan.TabIndex = 8;
+            this.daunhan.Text = "*";
+            this.daunhan.UseVisualStyleBackColor = true;
+            this.daunhan.Click += new System.EventHandler(this.nhappheptoan);
+            // 
+            // txtso1
+            // 
+            this.txtso1.Location = new System.Drawing.Point(165, 208);
+            this.txtso1.Name = "txtso1";
+            this.txtso1.Size = new System.Drawing.Size(66, 52);
+            this.txtso1.TabIndex = 7;
+            this.txtso1.Text = "1";
+            this.txtso1.UseVisualStyleBackColor = true;
+            this.txtso1.Click += new System.EventHandler(this.txtso1_Click);
+            // 
+            // txtso2
+            // 
+            this.txtso2.Location = new System.Drawing.Point(84, 208);
+            this.txtso2.Name = "txtso2";
+            this.txtso2.Size = new System.Drawing.Size(66, 52);
+            this.txtso2.TabIndex = 6;
+            this.txtso2.Text = "2";
+            this.txtso2.UseVisualStyleBackColor = true;
+            this.txtso2.Click += new System.EventHandler(this.txtso2_Click);
+            // 
+            // txtso3
+            // 
+            this.txtso3.Location = new System.Drawing.Point(12, 208);
+            this.txtso3.Name = "txtso3";
+            this.txtso3.Size = new System.Drawing.Size(66, 52);
+            this.txtso3.TabIndex = 5;
+            this.txtso3.Text = "3";
+            this.txtso3.UseVisualStyleBackColor = true;
+            this.txtso3.Click += new System.EventHandler(this.txtso3_Click);
+            // 
+            // dautru
+            // 
+            this.dautru.Location = new System.Drawing.Point(250, 150);
+            this.dautru.Name = "dautru";
+            this.dautru.Size = new System.Drawing.Size(66, 52);
+            this.dautru.TabIndex = 12;
+            this.dautru.Text = "-";
+            this.dautru.UseVisualStyleBackColor = true;
+            this.dautru.Click += new System.EventHandler(this.nhappheptoan);
+            // 
+            // txtso1so4
+            // 
+            this.txtso1so4.Location = new System.Drawing.Point(165, 145);
+            this.txtso1so4.Name = "txtso1so4";
+            this.txtso1so4.Size = new System.Drawing.Size(66, 52);
+            this.txtso1so4.TabIndex = 11;
+            this.txtso1so4.Text = "4";
+            this.txtso1so4.UseVisualStyleBackColor = true;
+            this.txtso1so4.Click += new System.EventHandler(this.txtso1so4_Click);
+            // 
+            // txtso5
+            // 
+            this.txtso5.Location = new System.Drawing.Point(84, 145);
+            this.txtso5.Name = "txtso5";
+            this.txtso5.Size = new System.Drawing.Size(66, 52);
+            this.txtso5.TabIndex = 10;
+            this.txtso5.Text = "5";
+            this.txtso5.UseVisualStyleBackColor = true;
+            this.txtso5.Click += new System.EventHandler(this.txtso5_Click);
+            // 
+            // txtso6
+            // 
+            this.txtso6.Location = new System.Drawing.Point(8, 145);
+            this.txtso6.Name = "txtso6";
+            this.txtso6.Size = new System.Drawing.Size(66, 52);
+            this.txtso6.TabIndex = 9;
+            this.txtso6.Text = "6";
+            this.txtso6.UseVisualStyleBackColor = true;
+            this.txtso6.Click += new System.EventHandler(this.txtso6_Click);
+            // 
+            // daucong
+            // 
+            this.daucong.Location = new System.Drawing.Point(250, 87);
+            this.daucong.Name = "daucong";
+            this.daucong.Size = new System.Drawing.Size(66, 52);
+            this.daucong.TabIndex = 16;
+            this.daucong.Text = "+";
+            this.daucong.UseVisualStyleBackColor = true;
+            this.daucong.Click += new System.EventHandler(this.nhappheptoan);
+            // 
+            // txtso7
+            // 
+            this.txtso7.Location = new System.Drawing.Point(167, 87);
+            this.txtso7.Name = "txtso7";
+            this.txtso7.Size = new System.Drawing.Size(66, 52);
+            this.txtso7.TabIndex = 15;
+            this.txtso7.Text = "7";
+            this.txtso7.UseVisualStyleBackColor = true;
+            this.txtso7.Click += new System.EventHandler(this.txtso7_Click);
+            // 
+            // txtso8
+            // 
+            this.txtso8.Location = new System.Drawing.Point(84, 87);
+            this.txtso8.Name = "txtso8";
+            this.txtso8.Size = new System.Drawing.Size(66, 52);
+            this.txtso8.TabIndex = 14;
+            this.txtso8.Text = "8";
+            this.txtso8.UseVisualStyleBackColor = true;
+            this.txtso8.Click += new System.EventHandler(this.txtso8_Click);
+            // 
+            // txtso9
+            // 
+            this.txtso9.Location = new System.Drawing.Point(8, 87);
+            this.txtso9.Name = "txtso9";
+            this.txtso9.Size = new System.Drawing.Size(66, 52);
+            this.txtso9.TabIndex = 13;
+            this.txtso9.Text = "9";
+            this.txtso9.UseVisualStyleBackColor = true;
+            this.txtso9.Click += new System.EventHandler(this.txtso9_Click);
+            // 
+            // maytinh
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(325, 343);
+            this.Controls.Add(this.daucong);
+            this.Controls.Add(this.txtso7);
+            this.Controls.Add(this.txtso8);
+            this.Controls.Add(this.txtso9);
+            this.Controls.Add(this.dautru);
+            this.Controls.Add(this.txtso1so4);
+            this.Controls.Add(this.txtso5);
+            this.Controls.Add(this.txtso6);
+            this.Controls.Add(this.daunhan);
+            this.Controls.Add(this.txtso1);
+            this.Controls.Add(this.txtso2);
+            this.Controls.Add(this.txtso3);
+            this.Controls.Add(this.dauchia);
+            this.Controls.Add(this.daubang);
+            this.Controls.Add(this.txtso0);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.txtketqua);
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.Name = "maytinh";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "maytinhcuanam";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label txtketqua;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button txtso0;
+        private System.Windows.Forms.Button daubang;
+        private System.Windows.Forms.Button dauchia;
+        private System.Windows.Forms.Button daunhan;
+        private System.Windows.Forms.Button txtso1;
+        private System.Windows.Forms.Button txtso2;
+        private System.Windows.Forms.Button txtso3;
+        private System.Windows.Forms.Button dautru;
+        private System.Windows.Forms.Button txtso1so4;
+        private System.Windows.Forms.Button txtso5;
+        private System.Windows.Forms.Button txtso6;
+        private System.Windows.Forms.Button daucong;
+        private System.Windows.Forms.Button txtso7;
+        private System.Windows.Forms.Button txtso8;
+        private System.Windows.Forms.Button txtso9;
+    }
+}
+
